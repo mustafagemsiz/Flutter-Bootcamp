c36f6ef4cf233b1633b56bd7f27f4eb4a40faa89
Mustafa Birhat Gemsiz <birhatgemsiz@hotmail.com>
2021-12-10 16:32:44 +0300

    edit

--- yemek_fikirleri/lib/main.dart
+++ yemek_fikirleri/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,24 +11,78 @@ class BenimUygulamam extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
+      home: SafeArea(
+        child: Scaffold(
           backgroundColor: Colors.white,
-          centerTitle: true,
-          title: const Text(
-            'Bugün ne yemek yapsam?',
-            style: TextStyle(color: Colors.black),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            centerTitle: true,
+            title: const Text(
+              'Rastgele Menü 🎲',
+              style: TextStyle(color: Colors.black),
+            ),
           ),
+          body: const YemekListesi(),
         ),
-        body: const YemekListesi(),
       ),
     );
   }
 }
 
-class YemekListesi extends StatelessWidget {
+class YemekListesi extends StatefulWidget {
   const YemekListesi({Key? key}) : super(key: key);
+
+  @override
+  State<YemekListesi> createState() => _YemekListesiState();
+}
+
+class _YemekListesiState extends State<YemekListesi> {
+  int tatliNo = 6, corbaNo = 6, yemekNo = 6;
+
+  List<String> corbaAdlari = [
+    'Mercimek',
+    'Tarhana',
+    'Tavuksuyu',
+    'Düğün Çorbası',
+    'Yoğurtlu Çorba',
+    ''
+  ];
+
+  List<String> yemekAdlari = [
+    'Karnıyarık',
+    'Mantı',
+    'Kuru Fasulye',
+    'İçli Köfte',
+    'Izgara Balık',
+    ''
+  ];
+
+  List<String> tatliAdlari = [
+    'Kadayıf',
+    'Baklava',
+    'Sütlaç',
+    'Kazandibi',
+    'Dondurma',
+    ''
+  ];
+
+  void tatliGetir() {
+    setState(() {
+      tatliNo = Random().nextInt(5) + 1;
+    });
+  }
+
+  void corbaGetir() {
+    setState(() {
+      corbaNo = Random().nextInt(5) + 1;
+    });
+  }
+
+  void yemekGetir() {
+    setState(() {
+      yemekNo = Random().nextInt(5) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,10 +97,19 @@ class YemekListesi extends StatelessWidget {
                     primary: Colors.white,
                     backgroundColor: Colors.white,
                   ),
-                  onPressed: () {
-                    print('Tatlı butonu tıklandı.');
-                  },
-                  child: Image.asset('assets/tatli_3.jpg')),
+                  onPressed: tatliGetir,
+                  child: Image.asset('assets/tatli_$tatliNo.jpg')),
+            ),
+          ),
+          Text(
+          tatliAdlari[tatliNo-1]
+          ,style: const TextStyle(color: Colors.black),
+          ),
+          const SizedBox(
+            width: 200,
+            child: Divider(
+              color: Colors.black,
+              height: 5,
             ),
           ),
           Expanded(
@@ -56,10 +120,19 @@ class YemekListesi extends StatelessWidget {
                     backgroundColor: Colors.white,
                     primary: Colors.white,
                   ),
-                  onPressed: () {
-                    print('Çorba butonu tıklandı.');
-                  },
-                  child: Image.asset('assets/corba_2.jpg')),
+                  onPressed: corbaGetir,
+                  child: Image.asset('assets/corba_$corbaNo.jpg')),
+            ),
+          ),
+          Text(
+            corbaAdlari[corbaNo - 1],
+            style: const TextStyle(color: Colors.black),
+          ),
+          const SizedBox(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
             ),
           ),
           Expanded(
@@ -70,10 +143,19 @@ class YemekListesi extends StatelessWidget {
                     backgroundColor: Colors.white,
                     primary: Colors.white,
                   ),
-                  onPressed: () {
-                    print('Mantı butonu tıklandı.');
-                  },
-                  child: Image.asset('assets/yemek_2.jpg')),
+                  onPressed: yemekGetir,
+                  child: Image.asset('assets/yemek_$yemekNo.jpg')),
+            ),
+          ),
+          Text(
+            yemekAdlari[yemekNo-1]
+          ,style: const TextStyle(color: Colors.black),
+          ),
+          const SizedBox(
+            width: 200,
+            child: Divider(
+              height: 10,
+              color:Colors.black,
             ),
           ),
         ],
